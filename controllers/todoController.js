00ef33b1726f9331816d9845515cc673840e4b41
Precell <82501476+Precell@users.noreply.github.com>
2021-11-28 12:42:36 +0200

    styled the navbar

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,7 +1,23 @@
 module.exports = function(app){
     
-    app.get('blog', function(req, res){
-        res.render()
+    app.get('/blog', function(req, res){
+        var blogs = [
+            {
+                title: "My First Blog",
+                content:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci et commodi quidem nostrum fugiat amet esse laudantium, aliquam sint inventore optio non! Officiis placeat laborum ex omnis at assumenda, 
+                repellat asperiores similique officia! Possimus!`,
+                date: Date()
+     
+             },
+             {
+                title: "My First Blog",
+                content:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci et commodi quidem nostrum fugiat amet esse laudantium, aliquam sint inventore optio non! Officiis placeat laborum ex omnis at assumenda, 
+                repellat asperiores similique officia! Possimus!`,
+                date: Date()
+     
+             }
+    ]
+        res.render('blog', {blogs:blogs})
     })
 
     app.post('/blog', function(req, res){
