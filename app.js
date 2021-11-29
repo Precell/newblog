ae69f4e536c6ce35fdbdb8aef8f1aa7fd3a87ac5
Precell <82501476+Precell@users.noreply.github.com>
2021-11-29 11:40:12 +0200

    heroku setup

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.use(express.static('./public'))
 todoController(app, urlParser)
 
 // LISTEN TO A PORT
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
 console.log('you are listening to port 3000')
 
