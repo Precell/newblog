9e8a7e88c2df414ea40aa0c34a3daff382d9fbae
Precell <82501476+Precell@users.noreply.github.com>
2021-12-01 14:28:51 +0200

    connected mongoDb handled routes

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,31 +1,56 @@
+var mongoose = require('mongoose');
+// CONNECT TO THE DATABASE
+mongoose.connect('mongodb+srv://precell:precell12345@cluster0.rfhax.mongodb.net/mongodb?retryWrites=true&w=majority');
+
+// CREATE SCHEMA - a blueprint for our data 
+var blogSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+
+// create a model or collection for our schema
+var Blog = mongoose.model('Blog', blogSchema);
+
+// var newBlog = Blog({title:'Precell', content:'Building an app'}).save(function(err){
+//     if(err) {throw err};
+//     console.log('item saved');
+// })
+
 module.exports = function(app, urlParser){
     
+//     var blogs = [
+//         {
+//             title: "My First dummy Blog",
+//             content:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci et commodi quidem nostrum fugiat amet esse laudantium, aliquam sint inventore optio non! Officiis placeat laborum ex omnis at assumenda, 
+//             repellat asperiores similique officia! Possimus!`,
+//             id: 3
+ 
+//          },
+//          {
+//             title: "My second dummey blog",
+//             content:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci et commodi quidem nostrum fugiat amet esse laudantium, aliquam sint inventore optio non! Officiis placeat laborum ex omnis at assumenda, 
+//             repellat asperiores similique officia! Possimus!`,
+//             id: 3
+ 
+//          }
+//          ,
+//          {
+//             title: "My third dummy blog",
+//             content:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci et commodi quidem nostrum fugiat amet esse laudantium, aliquam sint inventore optio non! Officiis placeat laborum ex omnis at assumenda, 
+//             repellat asperiores similique officia! Possimus!`,
+//             id: 3
+ 
+//          }
+// ]
+
+
     app.get('/', function(req, res){
-        var blogs = [
-            {
-                title: "My First dummy Blog",
-                content:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci et commodi quidem nostrum fugiat amet esse laudantium, aliquam sint inventore optio non! Officiis placeat laborum ex omnis at assumenda, 
-                repellat asperiores similique officia! Possimus!`,
-                date: Date()
-     
-             },
-             {
-                title: "My second dummey blog",
-                content:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci et commodi quidem nostrum fugiat amet esse laudantium, aliquam sint inventore optio non! Officiis placeat laborum ex omnis at assumenda, 
-                repellat asperiores similique officia! Possimus!`,
-                date: Date()
-     
-             }
-             ,
-             {
-                title: "My third dummy blog",
-                content:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci et commodi quidem nostrum fugiat amet esse laudantium, aliquam sint inventore optio non! Officiis placeat laborum ex omnis at assumenda, 
-                repellat asperiores similique officia! Possimus!`,
-                date: Date()
-     
-             }
-    ]
-        res.render('blog', {blogs:blogs})
+    //    GET DATA FROM MONGOdb AND PASS IT TO THE VIEW
+        Blog.find({}, function(err, data){
+            if(err) throw err;
+            res.render('blog', {blogs:data})
+
+        })
     })
 
     app.get('/newBlog', (req, res)=>{
@@ -33,6 +58,11 @@ module.exports = function(app, urlParser){
     })
 
     app.post('/blog', urlParser, function(req, res){
+        // get data from the view to mongodb
+        var newblog = Blog(req.body).save(function(err, data){
+            if(err) throw err;
+            res.json(data)
+        })
         // console.log(req.body)
     })
 
